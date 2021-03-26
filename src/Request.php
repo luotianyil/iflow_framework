a78f8615ca530be4d0e9861410f5f941b7330602
洛水天依 <2461082216@qq.com>
2021-03-26 14:42:26 +0800

    first commit

--- src/Request.php
+++ src/Request.php
@@ -18,7 +18,7 @@ class Request
     {
         $this->request = $request;
         $this->server = $request -> server;
-        $this->request_uri = $request -> server['path_info'] ?? $request -> server['request_uri'];
+        $this->request_uri = $this->request_uri = explode('?', $request -> server['path_info'] ?? $request -> server['request_uri'])[0];;
         $this->query_string = $request -> server['query_string'] ?? '';
         $this->request_method = $request -> server['request_method'];
 
